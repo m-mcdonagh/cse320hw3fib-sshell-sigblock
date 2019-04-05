f63c3e7248f8d51828c274c090c5ecd1a7c9127c
Michael McDonagh VM <mjmcdonagh@cs.stonybrook.edu>
2019-04-05 02:40:50 +0000

    Shell operation with command parsing foundation, and functions for manipulating shell's data structure (artist_ds.c)

--- src/artist_manager.c
+++ src/artist_manager.c
@@ -1,10 +1,48 @@
-#include <stdio.h>
 #include "defs.h"
+#include "shell.h"
+#include <stdio.h>
+#define equals(s1, s2) !strcmp(s1, s2)
+#define BOOLEAN char
+#define TRUE 1
+#define FALSE 0
 
-int main(void) {
-    char* msg = "Hello, world!\n";
+char* helpstring = "help\t\tprints this very message!\n\
+date\t\tprints the current date\n\
+hire N\t\thires N many artists (creates processes for each)\n\
+fire X\t\tfires the given artist X (kills process with PID X)\n\
+fireall\t\tfires all artists (kills all child processes)\n\
+assign X\tmarks the given artist X as assigned to a job (and the child process X prints it is assigned)\n\
+withdraw X\tmarks the given artist X as unassigned (and the child process X prints it is withdrawn)\n\
+list\t\tprints all artists in the form \"PID STATUS\" (ASSIGNED or WAITING)\n\
+exit\t\texits the shell\n";
 
-    cse320_print(msg);
+BOOLEAN executeCommand(char** args){
+	if (!args || !*args)
+		return TRUE;
+	if (equals(*args, "help")){
+		cse320_print( helpstring );
+	}
+	if (equals(*args, "date")){
+	}
+	if (equals(*args, "hire")){
+	}
+	if (equals(*args, "fire")){
+	}
+	if (equals(*args, "fireall")){
+	}
+	if (equals(*args, "assign")){
+	}
+	if (equals(*args, "withdraw")){
+	}
+	if (equals(*args, "list")){
+	}
+	if (equals(*args, "exit")){
+		return FALSE;
+	}
+	return TRUE;
+}
 
-    return 0;
+int main(void){
+	shell_loop(32);
+	exit(0);
 }
